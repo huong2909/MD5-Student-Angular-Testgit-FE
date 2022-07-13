88f73d515d35143504117f140a1c32f450670b9d
huong2909 <99933730+huong2909@users.noreply.github.com>
2022-07-14 00:47:26 +0700

    test formbuilding

--- src/app/student/student-create/student-create.component.ts
+++ src/app/student/student-create/student-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Clazz} from '../../model/Clazz';
 import {StudentService} from '../../service/student.service';
 import {ClazzService} from '../../service/clazz.service';
 import {Router} from '@angular/router';
+import {Student} from '../../model/Student';
 
 @Component({
   selector: 'app-student-create',
@@ -11,37 +12,43 @@ import {Router} from '@angular/router';
   styleUrls: ['./student-create.component.css']
 })
 export class StudentCreateComponent implements OnInit {
-  studentForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    score: new FormControl(),
-    age: new FormControl(),
-    clazzId: new FormControl(),
-  });
+  studentForm: FormGroup;
+
+    // = new FormGroup({
+    // name: new FormControl(),
+    // score: new FormControl(),
+    // age: new FormControl(),
+    // clazzId: new FormControl(),
+  // });
   obj: any;
   listClazz: Clazz[] = [];
   constructor(private studentService: StudentService,
               private clazzService: ClazzService,
-              private router: Router) { }
+              private router: Router,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.studentForm = this.fb.group({
+      id: [''],
+      name: ['', Validators.required],
+      score: ['', Validators.required],
+      age: [''],
+      clazz: ['']
+    });
     this.clazzService.getAll().subscribe((data) => {
       this.listClazz = data;
     });
   }
   submit() {
-    this.obj = {
-      name: this.studentForm.value.name,
-      age: this.studentForm.value.age,
-      score: this.studentForm.value.score,
-      clazz: {
-        id: this.studentForm.value.clazzId
-      }
-    };
-    this.studentService.save(this.obj).subscribe(() => {
-      this.router.navigate(['/student']);
+    console.log(this.studentForm.value);
+    this.studentService.save(this.studentForm.value).subscribe((student: Student) => {
+      this.studentForm.reset();
       alert('Thành công');
     }, error => {
       alert('Lỗi');
     }) ;
+  }
+  goBack() {
+    this.router.navigate(['/student']);
   }
 }
