55414f8a1b723f4c3e021c609a8477d521010c65
huong2909 <99933730+huong2909@users.noreply.github.com>
2022-07-14 00:18:14 +0700

    refactor code

--- src/app/service/student.service.ts
+++ src/app/service/student.service.ts
@@ -1,36 +1,47 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Student} from '../model/Student';
+
 const API_URL = 'http://localhost:8081/api/students';
+
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
+
   getAll(): Observable<Student[]> {
     return this.httpClient.get<Student[]>(API_URL);
   }
+
   save(student): Observable<Student> {
     return this.httpClient.post<Student>(API_URL, student);
   }
+
   getById(id): Observable<Student> {
-    return  this.httpClient.get<Student>(API_URL + `/${id}`);
+    return this.httpClient.get<Student>(API_URL + `/${id}`);
   }
+
   delete(id: number): Observable<Student> {
     return this.httpClient.delete<Student>(API_URL + `/${id}`);
   }
+
   update(id: number, student: Student): Observable<Student> {
     return this.httpClient.put<Student>(`${API_URL}/${id}`, student);
   }
+
   getByName(name): Observable<Student[]> {
-    return  this.httpClient.get<Student[]>(API_URL + `/search-by-name?name=${name}`);
+    return this.httpClient.get<Student[]>(API_URL + `/search-by-name?name=${name}`);
   }
+
   getByClazzId(id): Observable<Student[]> {
-    return  this.httpClient.get<Student[]>(API_URL + `/clazz?id=${id}`);
+    return this.httpClient.get<Student[]>(API_URL + `/clazz?id=${id}`);
   }
+
   getByScore(from, to): Observable<Student[]> {
-    return  this.httpClient.get<Student[]>(API_URL + `/score-between?from=${from}&&to=${to}`);
+    return this.httpClient.get<Student[]>(API_URL + `/score-between?from=${from}&&to=${to}`);
   }
 }
